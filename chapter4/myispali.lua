800dbcf21b28384e93997a0c8c36549998aa4888
uddeepk <uddeepk@gmail.com>
2021-05-16 17:08:12 -0800

    used indexes

--- chapter4/myispali.lua
+++ chapter4/myispali.lua
@@ -1,4 +1,11 @@
 -- defines a function to check if a string is palindrome
 function myispali(s)
-   return s == string.reverse(s)
+   for i = 1, #s//2 do
+      if string.byte(s, i) ~= string.byte(s, #s - (i - 1)) then
+	 return false
+      end
+   end
+   return true
+
+--   return s == string.reverse(s)
 end

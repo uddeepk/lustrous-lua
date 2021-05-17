a17e4a8595973277acf17a3ba44da60bf3dca691
uddeepk <uddeepk@gmail.com>
2021-05-16 17:09:31 -0800

    used negative indexes :)

--- chapter4/myispali.lua
+++ chapter4/myispali.lua
@@ -1,7 +1,7 @@
 -- defines a function to check if a string is palindrome
 function myispali(s)
    for i = 1, #s//2 do
-      if string.byte(s, i) ~= string.byte(s, #s - (i - 1)) then
+      if string.byte(s, i) ~= string.byte(s, -i) then
 	 return false
       end
    end

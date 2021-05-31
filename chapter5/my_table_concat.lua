5fe623a8d6f8443a9cfa6497d38f6c1254e30498
uddeepk <uddeepk@gmail.com>
2021-05-30 18:36:44 -0800

    failed attempt at optimize my table concat

--- chapter5/my_table_concat.lua
+++ chapter5/my_table_concat.lua
@@ -6,12 +6,39 @@ function my_table_concat(l)
    dofile("my_is_sequence.lua")
    s = ""
    if my_is_sequence(l) then
+      --[[ --the commen
       for _, v in ipairs(l) do
 	 s = s .. v
       end
+      return s
+	 
+      --]]
+      str = {s = ""}
+      --return give_string(l, 1)
+      give_string(l, 1, str)
+      return str.s
    else
       print("Not valid sequence")
    end
-   return s
+--   return nil
 end
 
+-- trying tail calls
+function give_string(l, n)
+   if n > #l then
+      return ""
+   end
+   -- else
+
+   return l[n] .. give_string(l, n + 1)
+end
+
+-- attempted to somehow pass string by ref...
+function give_string(l, n, r)
+   if n > #l then
+      return nil
+   end
+
+   r.s = r.s .. l[n]
+   give_string(l, n+1, r)
+end
